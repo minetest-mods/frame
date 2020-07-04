e7bfd6cfc038257db0b09abdef56fa24de271b7e
PetiAPocok <mmandna@gmail.com>
2020-07-04 13:50:31 +0200

    Rearranged

--- init.lua
+++ init.lua
@@ -212,11 +212,13 @@ minetest.register_craft({
 local items_to_frame = {}
 
 for _, node in pairs(minetest.registered_items) do
-    if (minetest.get_item_group(node.name, "not_in_creative_inventory") == 0
-        and node.drawtype ~= "airlike" and node.drawtype
-        ~= "nodebox" and node.drawtype ~= "mesh")
-        or ((node.drawtype == "nodebox"
-        or node.drawtype == "mesh") and node.inventory_image ~= "") then
+    if (minetest.get_item_group(node.name, "not_in_creative_inventory") == 0 and
+    node.drawtype ~= "airlike" and
+    node.drawtype ~= "nodebox" and
+    node.drawtype ~= "mesh") or
+    ((node.drawtype == "nodebox" or
+    node.drawtype == "mesh") and
+    node.inventory_image ~= "") then
         table.insert(items_to_frame, node.name)
     end
 end
