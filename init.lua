7adaa58649d58bf63fd7e4adc7af8c928587d60e
PetiAPocok <64635752+PetiAPocok@users.noreply.github.com>
2020-07-04 13:43:13 +0200

    Update init.lua
    Co-authored-by: Louis Royer <55180044+louisroyer@users.noreply.github.com>

--- init.lua
+++ init.lua
@@ -215,7 +215,6 @@ for i, node in pairs(minetest.registered_items) do
     if minetest.get_item_group(node.name, "not_in_creative_inventory") == 0
         and node.drawtype ~= "airlike" and node.drawtype
         ~= "nodebox" and node.drawtype ~= "mesh" then
-        items_to_frame[i] = node.name
         or ((node.drawtype == "nodebox"
         or node.drawtype == "mesh") and node.inventory_image ~= "" then
         table.insert(items_to_frame, node.name)
